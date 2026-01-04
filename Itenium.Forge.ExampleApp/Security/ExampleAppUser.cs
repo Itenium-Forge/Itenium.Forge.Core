874ac9118cfbcd42d59143fe325081a475135d89
Wouter Van Schandevijl <woutervs@hotmail.com>
2026-01-04 16:54:22 +0100

    Itenium.CodeStyle

--- Itenium.Forge.ExampleApp/Security/ExampleAppUser.cs
+++ Itenium.Forge.ExampleApp/Security/ExampleAppUser.cs
@@ -1,19 +1,18 @@
 using Itenium.Forge.Security;
 using System.Security.Claims;
 
-namespace Itenium.Forge.ExampleApp.Security
+namespace Itenium.Forge.ExampleApp.Security;
+
+public interface IExampleAppUser : ICurrentUser
 {
-    public interface IExampleAppUser : ICurrentUser
+    string Department { get; }
+}
+
+public class ExampleAppUser : CurrentUser, IExampleAppUser
+{
+    public ExampleAppUser(IHttpContextAccessor httpContextAccessor) : base(httpContextAccessor)
     {
-        string Department { get; }
     }
 
-    public class ExampleAppUser : CurrentUser, IExampleAppUser
-    {
-        public ExampleAppUser(IHttpContextAccessor httpContextAccessor) : base(httpContextAccessor)
-        {
-        }
-
-        public string Department => User?.FindFirstValue("department") ?? "Unknown";
-    }
+    public string Department => User?.FindFirstValue("department") ?? "Unknown";
 }
